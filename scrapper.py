78dc30c3cfa863f873e3017e5272322077ecea9a
Sameer Kumar <sameer18051998@gmail.com>
2017-01-06 20:16:35 +0530

    .

---
+++ scrapper.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def main():
+
+    base = "http://pricebaba.com/mobile/pricelist/android-phones-price-list?page="
+    count = 0
+    for i in range(0,49):
+        x = base+str(i)
+        mobile = {}
+        page = requests.get(x)
+        souped = BeautifulSoup(page.text,"lxml")
+
+        tag = souped.find_all('div')[0].find_all(class_='prdp')
+        #tag = souped.find_all(class_='carbrandpage')[0].find_all(class_='clearfix')
+
+
+        for tags in tag:
+            name = tags.find_all(class_='prdp-ttl')[0].text
+            price = tags.find_all(class_='prdp-prc')[0].text
+
+            if name in mobile.keys() or price == " - ":
+                pass
+            else:
+                print "%s costs%s" %(name, price)
+                count = count + 1
+
+    print "The total number of phones = %d" %(count)
+main()
