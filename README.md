93ca85ebe4f0c19dd6eef6de63bc89f1f5d44e59
Sameer Kumar <sameer18051998@gmail.com>
2017-01-07 02:48:26 +0530

    .

---
+++ README.md
@@ -0,0 +1,15 @@
+# PriceBaba.com-Scraping-Soup
+
+This project scrapes the name and price of all the 8 categories(mentioned below) from PriceBaba.com 
+It bascially prompts for an input in which it asks for the category of products.
+Also many other things can be scraped or various conditions can be imposed on it, such as minimum cost price or scraping any other data from PriceBaba
+
+Categories Scraped are - 
+1. Mobile
+2. Laptop
+3. Tablet
+4. Smartwatch
+5. Television
+6. Refrigerators
+7. Microwaves
+8. Washing-Machine
--- scrapper.py
+++ scrapper.py
@@ -1,14 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
+import copy
+#1- 2- 3- 4- 5- 6-
+def retrieval(input):
+    input = input -1
+    base1 = "http://pricebaba.com/"
+    base2 = "/pricelist/all-"
+    base3 = "-in-india?page="
+    category = ["Mobile","Laptop","Tablet","Smartwatch","television","refrigerators","Microwaves","Washing-Machine"]
+    new_category = copy.copy(category)
+    if input==0 or input==1:
+        new_category[input] = new_category[input] + "s-sold"
+
+    if input==2 or input==3 or input==4 or input==7:
+        new_category[input] = new_category[input] + "s"
+
+    if input==5:
+        new_category[input] = new_category[input] + "-sold"
 
 
-def main():
 
-    base = "http://pricebaba.com/mobile/pricelist/android-phones-price-list?page="
+    range_category = [48,31,10,3,21,13]
     count = 0
-    for i in range(0,49):
+<<<<<<< HEAD
+    for i in range(0,range_category[input]):
+        x = base1 + str(category[input]) + base2 + str(new_category[input]) + base3 + str(i)
+        item = {}
+=======
+    for i in range(0,48:
         x = base+str(i)
         mobile = {}
+>>>>>>> origin/master
         page = requests.get(x)
         souped = BeautifulSoup(page.text,"lxml")
 
@@ -20,11 +42,57 @@ def main():
             name = tags.find_all(class_='prdp-ttl')[0].text
             price = tags.find_all(class_='prdp-prc')[0].text
 
-            if name in mobile.keys() or price == " - ":
+            if name in item.keys() or price == " - ":
                 pass
             else:
                 print "%s costs%s" %(name, price)
                 count = count + 1
 
+<<<<<<< HEAD
+    print "\nThe total number of items listed = %d \n\n " %(count)
+
+def main():
+
+
+    input = int(raw_input("""Which category ? \n Press 1 for Mobiles \n Press 2 for Laptops
+                           \n Press 3 for Tablets \n Press 4 for Smartwatches \n Press 5 for TV's
+                          \n Press 6 for Refrigerators\n Press 7 for Microwaves \n Press 8 for Washing Machines \n \n """))
+
+    if input<=0 or input>8:
+        print "Error"
+        pass
+
+
+    if input == 1:
+        retrieval(input)
+
+    if input == 2:
+        retrieval(input)
+
+    if input == 3:
+        retrieval(input)
+
+    if input == 4:
+        retrieval(input)
+
+    if input == 5:
+        retrieval(input)
+
+    if input == 6:
+        retrieval(input)
+
+    if input == 7:
+        retrieval(input)
+
+    if input == 8:
+        retrieval(input)
+
+
+
+
+
+main()
+=======
     print "The total number of phones = %d" %(count)
 main()
+>>>>>>> origin/master
